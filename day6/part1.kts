5db68ae992227310e9d0fd3a42f066d5008390fb
Andrew Kirkegaard <andrew@farmlogs.com>
2016-12-12 12:29:07 -0500

    Day 6

---
+++ day6/part1.kts
@@ -0,0 +1,11 @@
+val counts = Array<MutableMap<Char, Int>>(8, { mutableMapOf<Char, Int>() })
+
+System.`in`.bufferedReader().lineSequence().forEach {
+  it.forEachIndexed { i, it ->
+    val map = counts[i]
+    val count = map.get(it) ?: 0
+    map.put(it, count + 1)
+  }
+}
+
+println(counts.map { it.asSequence().maxBy { it.value }?.key }.joinToString(""))
